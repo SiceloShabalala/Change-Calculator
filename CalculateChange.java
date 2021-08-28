841e69ad074d94e96de9d24663b326d5276e841c
SiceloShabalala <81635235+SiceloShabalala@users.noreply.github.com>
2021-08-28 18:16:11 +0200

    Add files via upload

--- CalculateChange.java
+++ CalculateChange.java
@@ -1,3 +1,8 @@
+/**
+ * @author: Sicelokuhle Shabalala
+ * @date: 28/08/2021
+ * @title: Change Calculator
+ */
 import java.util.*;
 public class CalculateChange
 {
@@ -9,12 +14,16 @@ public class CalculateChange
         sc.close();
     }
 
+    /**
+     * @param money is the amount to be converted into a change of R5 
+     */
     public static void change(int money)
     {
         int numOfR5 = money/5;
         int numOfR2 = (money - 5*numOfR5)/2;
         int numOfR1 = money - (5*numOfR5 + 2*numOfR2);
 
+        System.out.println("The change:");
         System.out.println(numOfR5+" R5 coins");
         System.out.println(numOfR2+" R2 coins");
         System.out.println(numOfR1+" R1 coins");
